dac20eeb791b8bea091a609e73890ab567867bea
Bo-Hsiang Tseng <bht26@ulm.eng.cam.ac.uk>
2019-02-28 22:02:44 +0000

    rm redundant files

--- run.sh
+++
@@ -1,7 +0,0 @@
-model_type=cvae
-model_path=toy.pt
-log=toy.log
-res=toy.res
-
-python3 train.py --model_type=$model_type --model_path=$model_path > $log
-python3 test.py --model_type=$model_type --model_path=$model_path > $res
